6f71c8a9b59b4923cdbedc5d1ae0ce143b374f87
Sebastian Tixilema <sebastixi2002@gmail.com>
2023-12-19 13:59:54 -0500

    Control de administrador

--- src/app/core/services/aside-menu.service.ts
+++ src/app/core/services/aside-menu.service.ts
@@ -18,10 +18,23 @@ export class AsideMenuService {
   para permitir la comunicación entre componentes:*/
   private _showMenu= new Subject<boolean>();
   showMenu= this._showMenu.asObservable();
-
+  obtenerUsuarioToken() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const payloadBase64 = token.split('.')[1];
+      const decodedPayload = atob(payloadBase64);
+      const payload = JSON.parse(decodedPayload);
+      const userName = payload.userName;
+      console.log('UserName:', userName);
+      return userName
+    } else {
+      console.error('No se encontró el token en localStorage.');
+      return null
+    }
+  }
 
   //lista de Menu con todo lo que se requiere
-  private asideMenuList: IAsideMenu[] = [
+  private asideMenuAdminList: IAsideMenu[] = [
     { title: 'Panel', url: '/panel', icon: 'grid_view' },
     { title: 'Agregar Usuario', url: '/usuarios', icon: 'person_add' },
     { title: 'Agregar Proveedor', url: '/proveedores', icon: 'apartment' },
@@ -30,19 +43,45 @@ export class AsideMenuService {
     { title: 'Salida De Productos', url: '/salida', icon: 'point_of_sale' },
     { title: 'Reportes', url: '/reportes', icon: 'report_gmailerrorred' }
   ];
+  private asideMenuList: IAsideMenu[] = [
+    { title: 'Panel', url: '/panel', icon: 'grid_view' },
+    { title: 'Agregar Proveedor', url: '/proveedores', icon: 'apartment' },
+    { title: 'Gestion Categorias', url: '/categorias', icon: 'category' },
+    { title: 'Registro De Productos', url: '/registro', icon: 'inventory' },
+    { title: 'Salida De Productos', url: '/salida', icon: 'point_of_sale' },
+    { title: 'Reportes', url: '/reportes', icon: 'report_gmailerrorred' }
+  ];
 
-  constructor() { }
+  constructor() { 
+    
+  }
 
   //@getAsideMenu(): me retorna una copiar de toda la lista
   getAsideMenu() {
-    return [...this.asideMenuList];
+    var usuario = this.obtenerUsuarioToken()
+    if (usuario && usuario=="ariel") {
+      return [...this.asideMenuAdminList];
+    } else{
+      return [...this.asideMenuList];
+    }
+    
   }
 
   //@getMenuByUrl: busca el menu por la URL especificada
   getAsideMenuByUrl(url:string):IAsideMenu{
-    return this.asideMenuList.find(
-      (menu)=>menu.url.toLowerCase() === url.toLowerCase()
-    )as IAsideMenu
+    
+    var usuario = this.obtenerUsuarioToken()
+    if (usuario && usuario=="ariel") {
+      return this.asideMenuAdminList.find(
+        (menu)=>menu.url.toLowerCase() === url.toLowerCase()
+      )as IAsideMenu
+    } else{
+      return this.asideMenuList.find(
+        (menu)=>menu.url.toLowerCase() === url.toLowerCase()
+      )as IAsideMenu
+    }
+    
   }  
+  
 
 }
